9bac6b1a824abf3a277679df751cd08b2c2352d3
yqf3139 <yqf3139@gmail.com>
2014-03-17 22:23:58 +0800

    src init

---
+++ main/java/com/seu/hitrip/card/newsTextCard.java
@@ -0,0 +1,99 @@
+package com.seu.hitrip.card;
+
+import android.content.Context;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.chris.lr.slidemenu.R;
+
+import it.gmariotti.cardslib.library.internal.Card;
+import it.gmariotti.cardslib.library.internal.CardExpand;
+import it.gmariotti.cardslib.library.internal.CardHeader;
+import it.gmariotti.cardslib.library.internal.base.BaseCard;
+import it.gmariotti.cardslib.library.view.CardView;
+
+/**
+ * Created by yqf on 3/17/14.
+ */
+public class newsTextCard extends Card {
+
+    private final String people;
+    private final String content;
+
+    public static Card getCard(final Context context, String people, String content){
+        newsTextCard card = new newsTextCard(context, people, content);
+
+        //Create a CardHeader
+        CardHeader header = new CardHeader(context);
+
+        header.setButtonExpandVisible(true);
+
+        header.setPopupMenu(R.menu.main, new CardHeader.OnClickCardHeaderPopupMenuListener(){
+            @Override
+            public void onMenuItemClick(BaseCard card, MenuItem item) {
+                Toast.makeText(context, "Click on " + item.getTitle(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        //Add Header to card
+        card.addCardHeader(header);
+
+        //Set onClick listener
+        card.setOnClickListener(new Card.OnCardClickListener() {
+            @Override
+            public void onClick(Card card, View view) {
+                Toast.makeText(context,"Clickable card", Toast.LENGTH_LONG).show();
+
+                card.setTitle("New Title");
+
+                // CardView cardView = (CardView) self.findViewById(R.id.carddemo);
+                ((CardView)view).refreshCard(card);
+            }
+        });
+
+        card.setInnerLayout(R.layout.card_inner_news_text);
+
+        //This provide a simple (and useless) expand area
+        CardExpand expand = new CardExpand(context);
+
+        //Set inner title in Expand Area
+        expand.setTitle("expend");
+
+        expand.setInnerLayout(R.layout.card_expend_text);
+
+        //Add expand to a card
+        card.addCardExpand(expand);
+
+        card.setExpanded(true);
+
+        card.setShadow(true);
+
+        return card;
+    }
+
+
+    private newsTextCard(Context context, String people, String content) {
+        super(context, R.layout.card_thumbnail_layout);
+        this.people = people;
+        this.content = content;
+    }
+
+    @Override
+    public void setupInnerViewElements(ViewGroup parent, View view) {
+
+        TextView newsTextView = (TextView) parent.findViewById(R.id.news_text);
+        TextView expendTextView = (TextView) view.findViewById(R.id.expend_text);
+
+        newsTextView.setText(people+content);
+        expendTextView.setText(people+content);
+
+
+        super.setupInnerViewElements(parent, view);
+    }
+
+
+
+}
---
+++ main/java/com/seu/hitrip/fragment/BaseFragment.java
@@ -0,0 +1,69 @@
+package com.seu.hitrip.fragment;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+import android.view.MenuItem;
+
+import com.chris.lr.slidemenu.R;
+
+
+/**
+ * Base Fragment
+ *
+ * @author Gabriele Mariotti (gabri.mariotti@gmail.com)
+ */
+public abstract class BaseFragment extends Fragment {
+
+    String title = null;
+    Integer titleId = 0;
+
+    public BaseFragment(){
+        titleId = getTitleResourceId();
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+
+        setTitle();
+    }
+
+    protected void setTitle(){
+        if(titleId == 0) return;
+        title = getResources().getString(titleId);
+        if(title == null) return;
+        getActivity().setTitle(title);
+    }
+
+    public abstract int getTitleResourceId();
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                // This ID represents the Home or Up button. In the case of this
+                // activity, the Up button is shown. Use NavUtils to allow users
+                // to navigate up one level in the application structure. For
+                // more details, see the Navigation pattern on Android Design:
+                //
+                // http://developer.android.com/design/patterns/navigation.html#up-vs-back
+                //
+                NavUtils.navigateUpFromSameTask(getActivity());
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
